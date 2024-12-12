33d1d565b56b521e527d2b25414c394e0a55566b
Julfried <julian.grimm8@gmail.com>
2024-12-12 13:38:24 +0100

    rotation

--- numpy_test.py
+++ numpy_test.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,4 +16,22 @@ subimg = stinkbug[0:frame_image_size.shape[0], 110:frame_image_size.shape[1]+110
 frame[size_x[0]:size_x[1], size_y[0]:size_y[1]] = subimg
 
 plt.imshow(frame, cmap="gray")
+plt.show()
+
+def transform(img_src, T):
+    img_dst = np.zeros_like(img_src)
+    for y in range(img_dst.shape[0]):
+        for x in range(img_dst.shape[1]):
+            new_coord = T @ np.array([y, x])
+            new_y = int(new_coord[0])
+            new_x = int(new_coord[1])
+            if 0 < new_y < img_dst.shape[0] and 0 < new_x < img_dst.shape[1]:
+                img_dst[y, x] = img_src[new_y, new_x]
+    return img_dst
+alpha = 30 # degrees
+T = np.array([[np.cos(math.radians(alpha)), -np.sin(math.radians(alpha))],
+              [np.sin(math.radians(alpha)), np.cos(math.radians(alpha))]])
+
+frame_rotated = transform(frame, T)
+plt.imshow(frame_rotated, cmap="gray")
 plt.show()
