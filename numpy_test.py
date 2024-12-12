815f614966e661dfb945aa2649ed83fca1354901
Julfried <julian.grimm8@gmail.com>
2024-12-12 12:56:15 +0100

    Stinkbug

--- numpy_test.py
+++ numpy_test.py
@@ -1,11 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-lst = [0,1,2,3,4,5,6,7,8,9]
-c = np.array(lst)
-
-print(lst)
-
 frame = r"C:\Users\Julian\OneDrive - FH OOe\Kurse\3. Semester\WPP3IL\images\frame_gray.png"
 stinkbug = r"C:\Users\Julian\OneDrive - FH OOe\Kurse\3. Semester\WPP3IL\images\stinkbug_gray.png"
 
@@ -15,7 +10,7 @@ stinkbug = plt.imread(stinkbug)
 frame = plt.imread(frame)
 frame_image_size = np.zeros_like(frame[size_x[0]:size_x[1], size_y[0]:size_y[1]])
 
-subimg = stinkbug[0:frame_image_size.shape[0], 0:frame_image_size.shape[1]]
+subimg = stinkbug[0:frame_image_size.shape[0], 110:frame_image_size.shape[1]+110]
 
 frame[size_x[0]:size_x[1], size_y[0]:size_y[1]] = subimg
 
