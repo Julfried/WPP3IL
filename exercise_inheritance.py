b28d995fe56ae599f40a0c0c641c185e206e817d
Julfried <julian.grimm8@gmail.com>
2025-01-17 09:39:16 +0100

    add GameEntity and Animal class hierarchy with display functionality

---
+++ exercise_inheritance.py
@@ -0,0 +1,36 @@
+class GameEntity:
+    def __init__(self, name, health): # constructor
+        self.name = name
+        self.health = health
+
+    def display_info(self):
+        print(f"{self.name} has health {self.health}")
+
+class Animal(GameEntity):
+    nr_instances = 0
+    def __init__(self, health):
+        Animal.nr_instances += 1
+        super().__init__(f"{Animal.nr_instances}-{self.__class__.__qualname__}", health)
+    
+class Cow(Animal):
+    def __init__(self):
+        super().__init__(10)
+
+class Parrot(Animal):
+    def __init__(self):
+        super().__init__(6)
+
+class Chicken(Animal):
+    def __init__(self):
+        super().__init__(4)
+
+if __name__ == "__main__":
+    animals = [
+        Cow(),
+        Parrot(),
+        Parrot(),
+        Chicken(),
+    ]
+
+    for animal in animals:
+        animal.display_info()
